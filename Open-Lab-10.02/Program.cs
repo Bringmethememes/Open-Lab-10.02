9dc85ae8b6fd150b2e1a6e7db70f727899268c89
PC <PC@ANDREJKOVPOITA>
2020-11-15 22:58:15 +0100

    Add project files.

---
+++ Open-Lab-10.02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Open_Lab_10._02
+{
+    class Program
+    {
+        static void Main()
+        {
+            Book LOTR = new Book();
+            LOTR.SetTitle("Plebovia a ich vyskyt, ");
+            LOTR.SetPages(888);
+            LOTR.SetAuthor(", Vladimir Iľjič Lenin, ");
+            LOTR.SetreleaseDate(2222);
+        }
+    }
+    class Book
+    {
+       public void SetTitle(string title)
+        {
+            Console.Write(title);
+        }
+        public void SetPages(int pages)
+        {
+            Console.Write(pages);
+        }
+        public void SetAuthor(string author)
+        {
+            Console.Write(author);
+        }
+        public void SetreleaseDate(int releaseDate)
+        {
+            Console.Write(releaseDate);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
